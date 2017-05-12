fcb48fb7bad1cde3d37d5cc84180c44aff322a2b
Jan Tristan Milan <jtbmilan@gmail.com>
2017-05-13 04:20:35 +0800

    Add the sidebar form for the stop generator app

--- preprocessor/models.py
+++ preprocessor/models.py
@@ -7,6 +7,7 @@ from django.db import models
 class Location(models.Model):
     name = models.CharField(max_length=250)
     path = models.CharField(max_length=500)
+    # TODO: Include default center of location bounds or compute dynamically from bounds
 
     def __str__(self):
         return self.name
