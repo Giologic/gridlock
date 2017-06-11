a431c4db2b4d5ecccd1b51fa1df3288c4ecc2da2
Jan Tristan Milan <jtbmilan@gmail.com>
2017-06-11 19:16:35 +0800

    Add exporting of current configuration

--- static/js/gtfs.js
+++ static/js/gtfs.js
@@ -17,9 +17,45 @@ function initializeGtfsManagement() {
      showDialog(exportDialogSettings, function() {
           $('#start-export-btn').click(function () {
               console.log("gtfs.js:initializeGtfsManagement: export of current state started");
+
+              var gtfsData = [];
+              if ($('#export-stops-checkbox').is(':checked')) {
+                  Array.prototype.push.apply(gtfsData, getStopsGtfs());
+              }
+              if ($('#export-routes-checkbox').is(':checked')) {
+                  Array.prototype.push.apply(gtfsData, getRoutesGtfs());
+              }
+
+              var blob = new Blob(gtfsData, {type: "text/plain;charset=utf-8"});
+              saveAs(blob, $('#export-filename-field').val());
+              removeActiveDialog();
           });
      });
   });
+}
+
+function getStopsGtfs() {
+    var stopsData = [];
+    stopsLayer.getLayers().forEach(function (s) {
+        stopsData.push("stop" + ", " + s._latlng.lat + ", "  + s._latlng.lng + ", " + s.options['fillColor'] + "\n");
+    });
+    return stopsData;
+}
+
+function getRoutesGtfs() {
+    var routesData = [];
+    routesLayer.getLayers().forEach(function (r) {
+        routesData.push("route" + ", " +  JSON.stringify(convertToLatlngTuple(r.getLatLngs())) + "\n");
+    });
+    return  routesData;
+}
+
+function convertToLatlngTuple(latLngs) {
+    var ret = [];
+    latLngs.forEach(function (l) {
+        ret.push([l.lat, l.lng]);
+    });
+    return ret;
 }
 
 function readSingleFile(e) {
