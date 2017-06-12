6415f8819662d1f6e5b7c6641f8273bdfa2ce9b1
Jan Tristan Milan <jtbmilan@gmail.com>
2017-06-12 21:35:53 +0800

    Add importing of existing configuration

--- static/js/gtfs.js
+++ static/js/gtfs.js
@@ -3,35 +3,72 @@ $(document).ready(function () {
 });
 
 function initializeGtfsManagement() {
-  $('#import-navbar-btn').click(function () {
-      console.log("gtfs.js:initialiGtfsManagement: import-navbar-btn clicked");
-      showDialog(importDialogSettings, function () {
-          $('#start-import-btn').click(function () {
-              console.log("gtfs.js:initializeGtfsManagement: import of existing state started");
-          });
-      });
-  });
 
-  $('#export-navbar-btn').click(function () {
-     console.log("gtfs.js:initialGtfsManagement: export-navbar-btn clicked");
-     showDialog(exportDialogSettings, function() {
-          $('#start-export-btn').click(function () {
-              console.log("gtfs.js:initializeGtfsManagement: export of current state started");
 
-              var gtfsData = [];
-              if ($('#export-stops-checkbox').is(':checked')) {
-                  Array.prototype.push.apply(gtfsData, getStopsGtfs());
-              }
-              if ($('#export-routes-checkbox').is(':checked')) {
-                  Array.prototype.push.apply(gtfsData, getRoutesGtfs());
-              }
+    $('#import-navbar-btn').click(function () {
+        console.log("gtfs.js:initialiGtfsManagement: import-navbar-btn clicked");
+        showDialog(importDialogSettings, function () {
+            $('#start-import-btn').click(function () {
+                console.log("gtfs.js:initializeGtfsManagement: import of existing state started");
 
-              var blob = new Blob(gtfsData, {type: "text/plain;charset=utf-8"});
-              saveAs(blob, $('#export-filename-field').val());
-              removeActiveDialog();
-          });
-     });
-  });
+                var configFileInput = document.getElementById("config-file-input");
+                var reader = new FileReader();
+                reader.onload = function (e) {
+                    var inputLines = reader.result.split("\n");
+                    inputLines.forEach(loadInput);
+                };
+
+                reader.readAsText(configFileInput.files[0]);
+            });
+        });
+    });
+
+    $('#export-navbar-btn').click(function () {
+        console.log("gtfs.js:initialGtfsManagement: export-navbar-btn clicked");
+        showDialog(exportDialogSettings, function() {
+            $('#start-export-btn').click(function () {
+                console.log("gtfs.js:initializeGtfsManagement: export of current state started");
+                var gtfsData = [];
+                if ($('#export-stops-checkbox').is(':checked')) {
+                    Array.prototype.push.apply(gtfsData, getStopsGtfs());
+                }
+                if ($('#export-routes-checkbox').is(':checked')) {
+                    Array.prototype.push.apply(gtfsData, getRoutesGtfs());
+                }
+
+                var blob = new Blob(gtfsData, {type: "text/plain;charset=utf-8"});
+                saveAs(blob, $('#export-filename-field').val());
+                removeActiveDialog();
+            });
+        });
+    });
+}
+
+function loadInput(inputStr) {
+    if (inputStr.match('stop, .*')) {
+        loadStopGtfs(inputStr);
+    } else if (inputStr.match('route, .*')) {
+        loadRouteGtfs(inputStr);
+    } else {
+        console.log("gtfs.js:loadinput: invalid input string read");
+    }
+}
+
+function loadStopGtfs(inputStr) {
+    console.log("gtfs.js:loadStopGtfs: stop has been loaded");
+    var data = inputStr.split(', ');
+    var lat = parseFloat(data[1]);
+    var lng = parseFloat(data[2]);
+    var color = data[3];
+    addStopNodeMarker(L.latLng(lat, lng), color);
+}
+
+function loadRouteGtfs(inputStr) {
+    console.log("gtfs.js:loadRouteGtfs: route has been loaded");
+    var data = inputStr.split(', ');
+    var routePoints = JSON.parse(data[1]);
+    var polyLine = L.polyline(routePoints);
+    routesLayer.addLayer(polyLine);
 }
 
 function getStopsGtfs() {
@@ -56,19 +93,4 @@ function convertToLatlngTuple(latLngs) {
         ret.push([l.lat, l.lng]);
     });
     return ret;
-}
-
-function readSingleFile(e) {
-  var file = e.target.files[0];
-  if (!file) {
-    return;
-  }
-
-  var reader = new FileReader();
-  reader.onload = function(e) {
-    var contents = e.target.result;
-    displayContents(contents);
-  };
-
-  reader.readAsText(file);
 }
